4a3d9793007a6c2673aee7d8dd54cddf64547b9b
Frank S. Thomas <frank@timepit.eu>
2015-07-12 22:57:05 +0200

    FieldNames predicate

--- shared/src/main/scala/eu/timepit/refined/generic.scala
+++ shared/src/main/scala/eu/timepit/refined/generic.scala
@@ -2,21 +2,16 @@ package eu.timepit.refined
 
 import eu.timepit.refined.InferenceRule.==>
 import eu.timepit.refined.generic._
-import shapeless.ops.hlist.ToTraversable
-import shapeless.ops.product.ToRecord
+import shapeless.ops.hlist.ToList
 import shapeless.ops.record.Keys
-import shapeless.ops.tuple.ToList
-import shapeless.record._
-
-import shapeless.{HList, LabelledGeneric, Witness}
+import shapeless.{ HList, LabelledGeneric, Witness }
 
 object generic extends GenericPredicates with GenericInferenceRules {
 
   /** Predicate that checks if a value is equal to `U`. */
   trait Equal[U]
 
-  // P ist z.B. StartsWith[W.`"foo"`.T]
-  trait FieldMatches[P]
+  trait FieldNames[P]
 
   /** Predicate that witnesses that the type of a value is a subtype of `U`. */
   trait Subtype[U]
@@ -30,26 +25,11 @@ private[refined] trait GenericPredicates {
   implicit def equalPredicate[T, U <: T](implicit wu: Witness.Aux[U]): Predicate[Equal[U], T] =
     Predicate.instance(_ == wu.value, t => s"($t == ${wu.value})")
 
-  implicit def fieldMatches[T, P, R <: HList, K <: HList](implicit lg: LabelledGeneric.Aux[T, R],
-                                              keys: Keys.Aux[R, K], ktl: ToTraversable.Aux[K, List, Any],
-                                                        tr: ToRecord.Aux[T, R],
-                                                         p: Predicate[P, String]): Predicate[FieldMatches[P], T] = {
-    Predicate.instance(t => {
-      //val x = tr.apply(t)
+  implicit def fieldNamesPredicate[T, P, R <: HList, K <: HList](implicit lg: LabelledGeneric.Aux[T, R],
+    keys: Keys.Aux[R, K], ktl: ToList[K, Any], p: Predicate[P, List[String]]): Predicate[FieldNames[P], T] = {
 
-      tr.apply(t).keys.toList.map(_.toString.tail).exists(p.isValid)
-     // val x = lg.to(t)
-
-
-      //keys.apply()
-
-      //val keys = Keys[lg.Repr]
-      //val x = ktl(keys())
-     //t.keys.toList
-      //val ks = keys().toList.map(_.toString.tail)
-      //ks.exists(p.isValid)
-
-    }, t => "")
+    val fieldNames = keys().toList.map(_.toString.drop(1))
+    Predicate.instance(_ => p.isValid(fieldNames), _ => p.show(fieldNames))
   }
 
   implicit def subtypePredicate[T, U >: T]: Predicate[Subtype[U], T] =
